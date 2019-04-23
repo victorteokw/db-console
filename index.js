b64ff11540b5c3ff3093c434ac1a2fa4b5bf847e
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-23 16:11:47 +0800

    version and help

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 const loadConfig = require('./lib/loadConfig');
+const showHelp = require('./lib/showHelp');
+const showVersion = require('./lib/showVersion');
 
 // Get config object
 const config = loadConfig();
+
+if (config.help) {
+  showHelp();
+  process.exit(0);
+}
+
+if (config.version) {
+  showVersion();
+  process.exit(0);
+}

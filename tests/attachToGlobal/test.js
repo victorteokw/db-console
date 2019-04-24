5731b87fa7282abe7e41413f8de8accbf8fb14e3
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-24 21:25:22 +0800

    attach to global test

---
+++ tests/attachToGlobal/test.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { assert } = require('chai');
+const attachToGlobal = require('../../lib/attachToGlobal');
+
+describe('Attach to global: ', function() {
+
+  it('attach an object key value pairs to global', function() {
+    assert.equal(global._unexist_random_name_111, undefined);
+    assert.equal(global._unexist_random_name_222, undefined);
+    assert.equal(global._unexist_random_name_333, undefined);
+    attachToGlobal({
+      _unexist_random_name_111: '1',
+      _unexist_random_name_222: '2',
+      _unexist_random_name_333: '3'
+    });
+    assert.equal(global._unexist_random_name_111, '1');
+    assert.equal(global._unexist_random_name_222, '2');
+    assert.equal(global._unexist_random_name_333, '3');
+  });
+
+});
--- tests/getModelFileList/test.js
+++ tests/getModelFileList/test.js
@@ -6,23 +6,40 @@ describe('Get model file list: ', function() {
 
   it('supports glob pattern', function() {
     const files = getModelFileList(__dirname, '**/*.ts', 'glob').map((f) => {
-      return path.relative(__dirname, f);
+      return path.relative(path.join(__dirname, 'glob'), f);
     });
     assert.deepEqual(files, [
-      'glob/file1.ts',
-      'glob/file2.ts',
-      'glob/subfolder/file1.ts'
+      'file1.ts',
+      'file2.ts',
+      'subfolder/file1.ts'
     ]);
   });
 
   it('supports regular expression', function() {
-    const files = getModelFileList(__dirname, '**/*.ts', 'glob').map((f) => {
-      return path.relative(__dirname, f);
-    });
+    const files = getModelFileList(__dirname, /^[a-z]+\.js$/, 'regexp')
+      .map((f) => {
+        return path.relative(path.join(__dirname, 'regexp'), f);
+      });
     assert.deepEqual(files, [
-      'glob/file1.ts',
-      'glob/file2.ts',
-      'glob/subfolder/file1.ts'
+      'abc.js',
+      'def.js',
+      'ghi.js'
+    ]);
+  });
+
+  it('supports array', function() {
+    const files = getModelFileList(
+      __dirname,
+      ['1.js', '3.ts', '6.json'],
+      'array'
+    )
+      .map((f) => {
+        return path.relative(path.join(__dirname, 'array'), f);
+      });
+    assert.deepEqual(files, [
+      '1.js',
+      '3.ts',
+      '6.json'
     ]);
   });
 });
